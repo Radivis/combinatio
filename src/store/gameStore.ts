bead17146a0a2ac5abe67e54d7cea6e81ae34e44
Radivis <radivis@radivis.com>
2023-07-16 14:10:09 +0200

    Disabling a color also removes it from the hints of colors possible in a slot

--- src/store/gameStore.ts
+++ src/store/gameStore.ts
@@ -558,6 +558,12 @@ const useGameStore = create<gameState & gameActions>()(
                     // set min and max of this color to 0
                     state.hints.colorsMinMax[colorIndex][0] = 0;
                     state.hints.colorsMinMax[colorIndex][1] = 0;
+                    // remove this color from the possibleSlotColors for all slots
+                    state.hints.possibleSlotColorsDataStrings.forEach((possibleSlotColorsDataString, index) => {
+                        const possibleSlotColors = Colors.deserialize(possibleSlotColorsDataString);
+                        possibleSlotColors.remove(color);
+                        state.hints.possibleSlotColorsDataStrings[index] = Colors.serialize(possibleSlotColors);
+                    })
                 }
                 state.hints.disabledColorsDataString = Colors.serialize(disabledColors);
             }, false, 'toggleDisableColor');

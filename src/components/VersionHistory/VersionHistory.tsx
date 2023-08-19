97467909d908d3bd03dc91750f5da91d4e0c5f67
Radivis <radivis@radivis.com>
2023-08-19 14:24:30 +0200

    Version history notes for V0.6.2

--- src/components/VersionHistory/VersionHistory.tsx
+++ src/components/VersionHistory/VersionHistory.tsx
@@ -3,6 +3,18 @@ import './VersionHistory.css';
 const VersionHistory = () => {
     return (
         <div>
+            <h2>V0.6.2</h2>
+            <ul className='version-history-list'>
+                <li>The "zanthia" color palette works again</li>
+                <li>The settings page now works as intended for the "icon" pieceType</li>
+                <li>Already filled rows in "icon" pieceType mode aren't colored</li>
+                <li>Icons within the combination notes are semi-transparent in "colorIcon" pieceType mode</li>
+                <li>Fixed the broken behavior of long press on mobile devices</li>
+                <li>Dragging a piece prevents its long press event from firing as long as it is dragged</li>
+                <li>Long press on a slot hint piece now selects the piece as the only one</li>
+                <li>Pieces can now also be dragged from the game board or the combination notes</li>
+                <li>When dragging pieces within the same row of the game board or the combination notes they switch position, if that option is enabled</li>
+            </ul>
             <h2>V0.6.1</h2>
             <ul className='version-history-list'>
                 <li>Added long-press functionality for game slots and combination hint slots</li>
